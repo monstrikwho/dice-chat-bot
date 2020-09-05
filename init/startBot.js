c660a08a004b3ab2d09cac363b633277d3d2bf23
dev.tarasiuk <sasha50475@gmail.com>
2020-09-05 17:58:55 +0300

    1

--- init/startBot.js
+++ init/startBot.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.MONGO_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Start bot
 async function startBot() {
