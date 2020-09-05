fd5dd4b71de77319901f06026d2e6a3977eb501f
dev.tarasiuk <sasha50475@gmail.com>
2020-09-05 10:49:20 +0300

    добавил простую стартовую проверку на регистрацию

--- commands/start.js
+++ commands/start.js
@@ -1,4 +1,5 @@
 const setupScenes = require("../scens/setupScenes");
+const User = require("../models/user");
 
 function setupStart(bot) {
   // Setup scens
@@ -6,6 +7,14 @@ function setupStart(bot) {
 
   // Start command
   bot.start(async (ctx) => {
+    const selectUser = await User.findOne({ userId: ctx.from.id });
+
+    if(selectUser) {
+      ctx.reply('Вы уже зарегистрированы.')
+      await ctx.scene.enter("showMainMenu");
+      return
+    }
+
     await ctx.scene.enter("step1");
   });
 }
