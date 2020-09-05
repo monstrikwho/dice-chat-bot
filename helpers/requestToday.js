6f525b510f1416d6885274d33018d9849c5da845
dev.tarasiuk <sasha50475@gmail.com>
2020-09-05 08:04:21 +0300

    1

--- helpers/requestToday.js
+++ helpers/requestToday.js
@@ -3,9 +3,28 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const querystring = require("querystring");
 const nodeHtmlToImage = require("node-html-to-image");
+const User = require("../models/user");
 
-const requestToday = (id) => {
-  return id
-}
+const getNowDay = require("./getNowDay");
 
-module.exports = requestToday
+const requestToday = async (id) => {
+  const selectUser = await User.findOne({ userId: id });
+
+  const nowDate = getNowDay();
+
+  console.log(selectUser.group);
+  console.log(nowDate);
+
+  // await axios.post(
+  //   `http://rasp.barsu.by/stud.php`,
+  //   querystring.stringify({
+  //     ft: 0,
+  //     sp: 0,
+  //     gp: statusId.group,
+  //     nd: "2020-05-25",
+  //     go: "Показать",
+  //   })
+  // );
+};
+
+module.exports = requestToday;
