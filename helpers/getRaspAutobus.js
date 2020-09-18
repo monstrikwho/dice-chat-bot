a7862d17de377b9ab32736caddb55fba3f0c6b64
dev.tarasiuk <sasha50475@gmail.com>
2020-09-18 22:50:49 +0300

    присылает правильно время

--- helpers/getRaspAutobus.js
+++ helpers/getRaspAutobus.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const { places, inUniversity, outUniversity } = require("../keyboards/autobus");
 
 module.exports.nearest = async (ctx, autobusObj, flag) => {
@@ -6,13 +7,14 @@ module.exports.nearest = async (ctx, autobusObj, flag) => {
   const minutesToday =
     date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
-  for(let num of Object.keys(autobusObj)) {
+  for (let num of Object.keys(autobusObj)) {
     let timeArr = flag === "in" ? inUniversity[num] : outUniversity[num];
     if (flag === "in") {
       const place = autobusObj[num];
       const placeKey = places[num].indexOf(place);
       timeArr = timeArr[placeKey];
     }
+
     const takeTime = [];
     for (let time of timeArr) {
       const [hours, minutes] = time.split(":");
@@ -24,6 +26,29 @@ module.exports.nearest = async (ctx, autobusObj, flag) => {
       }
     }
     const nearestTime = takeTime.slice(0, 3);
-    await ctx.reply(`${num} - ${nearestTime}`);
+
+    await ctx.reply(`${num} автобус
+${nearestTime
+  .map((item) => {
+    const [hours, minute] = item.split(":");
+
+    const diffHours = moment([2007, 0, 29, hours, minute, 00]).diff(
+      [2007, 0, 29, hoursToday, minutesToday, 00],
+      "hours"
+    );
+    const diffMinutes = moment([2007, 0, 29, hours, minute, 00]).diff(
+      [2007, 0, 29, hoursToday, minutesToday, 00],
+      "minutes"
+    );
+
+    if (diffHours !== 0) {
+      const restMinutes = diffMinutes - diffHours * 60;
+      return `через ${diffHours}:${restMinutes} часа  -  ${item}`;
+    } else {
+      return `через ${diffMinutes} минут  -  ${item}`;
+    }
+  })
+  .join(", \n")}
+    `);
   }
 };
