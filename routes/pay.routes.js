0651afbb211c6d7db8975d56701400a5a708d1db
dev.tarasiuk <sasha50475@gmail.com>
2020-11-09 23:14:57 +0300

    sdf

--- routes/pay.routes.js
+++ routes/pay.routes.js
@@ -3,16 +3,7 @@ const router = Router();
 
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body)
-    console.log(res.body)
-  } catch (error) {
-    req.status(500).json({ message: "Что-то пошло не так" });
-  }
-});
-router.get("/", async (req, res) => {
-  try {
-    console.log(req.body)
-    console.log(res.body)
+    console.log(req)
   } catch (error) {
     req.status(500).json({ message: "Что-то пошло не так" });
   }
