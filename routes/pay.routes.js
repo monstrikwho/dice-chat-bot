f7f77004e098e3faa74552ba9dfc8c513285caf5
dev.tarasiuk <sasha50475@gmail.com>
2020-11-10 01:30:03 +0300

    wer

--- routes/pay.routes.js
+++ routes/pay.routes.js
@@ -4,6 +4,7 @@ const router = Router();
 router.post("/", async (req, res) => {
   try {
     console.log(req.body)
+    req.status(200)
   } catch (error) {
     req.status(500).json({ message: "Что-то пошло не так" });
   }
