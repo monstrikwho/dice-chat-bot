8abf5b01bdf58ce7162a67018d4bb3d87693ca90
dev.tarasiuk <sasha50475@gmail.com>
2020-11-13 02:17:14 +0300

    sdf

--- init/startRoutes.js
+++ init/startRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const https = require("https");
 const cors = require("cors");
+const fs = require("fs");
 
-var app = express();
+const app = express();
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,13 +12,31 @@ app.use("/verify_pay", require("../routes/pay.routes"));
 
 async function startRoutes() {
   try {
-    app.listen(process.env.PORT, () =>
-      console.log(`Express has been started on port ${process.env.PORT}...`)
-    );
+    https
+      .createServer(
+        {
+          key: fs.readFileSync("./cert/server.key"),
+          cert: fs.readFileSync("./cert/server.crt"),
+        },
+        app
+      )
+      .listen(process.env.PORT, function () {
+        console.log(`Server listens on port: ${process.env.PORT}`);
+      });
   } catch (e) {
     console.log("Server Error", e.message);
     process.exit(1);
   }
 }
+// async function startRoutes() {
+//   try {
+//     app.listen(process.env.PORT, () =>
+//       console.log(`Express has been started on port ${process.env.PORT}...`)
+//     );
+//   } catch (e) {
+//     console.log("Server Error", e.message);
+//     process.exit(1);
+//   }
+// }
 
 module.exports = { app, startRoutes };
