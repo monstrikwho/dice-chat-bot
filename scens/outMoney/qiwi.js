183bce4fdd1f7cd8fae09a271875ea94af4f0240
dev.tarasiuk <sasha50475@gmail.com>
2020-11-13 19:14:10 +0300

    sdf

--- scens/outMoney/qiwi.js
+++ scens/outMoney/qiwi.js
@@ -49,8 +49,24 @@ outQiwi.on("text", async (ctx) => {
     },
   };
 
+  // await axios
+  //   .post(`https://edge.qiwi.com/sinap/api/v2/terms/99/payments`, obj)
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.log(err.message));
   await axios
-    .post(`https://edge.qiwi.com/sinap/api/v2/terms/99/payments`, obj)
+    .post(`https://edge.qiwi.com/sinap/providers/99/onlineCommission`, {
+      account: "79206020622",
+      paymentMethod: {
+        type: "Account",
+        accountId: "643",
+      },
+      purchaseTotals: {
+        total: {
+          amount: 1,
+          currency: "643",
+        },
+      },
+    })
     .then((res) => console.log(res))
     .catch((err) => console.log(err.message));
 
