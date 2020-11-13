2a536f7f3fa05856444aecb0884316f5634d96e6
dev.tarasiuk <sasha50475@gmail.com>
2020-11-13 18:49:54 +0300

    каыв

--- scens/outMoney/qiwi.js
+++ scens/outMoney/qiwi.js
@@ -2,6 +2,7 @@ const Scene = require("telegraf/scenes/base");
 const Extra = require("telegraf/extra");
 const Markup = require("telegraf/markup");
 const User = require("../../models/user");
+const axios = require("axios");
 
 // *************************** STEP 1 *******************************************
 const outQiwi = new Scene("outQiwi");
@@ -11,22 +12,36 @@ outQiwi.enter(async (ctx) => {
     mainBalance: user.mainBalance,
   };
   return await ctx.reply(
-    `Ваш баланс: ${user.mainBalance}
-Минималка на вывод 50р. Введите сумму вывода (цифру)`,
+    `Минималка на вывод 50р. Введите сумму вывода (цифру)`,
     Extra.markup(Markup.keyboard([["↪️ Вернуться назад"]]).resize())
   );
 });
 
 outQiwi.on("text", async (ctx) => {
   const count = +ctx.update.message.text.replace(/\D+/, "");
-  const balance = ctx.session.state.mainBalance
+  const balance = ctx.session.state.mainBalance;
 
   if (!Boolean(count)) return ctx.reply("Пожалуйста, введите только цифры.");
   if (count < 50) return ctx.reply("Минимальный вывода 50р.");
   if (count > balance)
     return ctx.reply("У вас недостаточно средств на балансе.");
 
-  
+  await axios.post(`https://edge.qiwi.com/sinap/api/v2/terms/99/payments`, {
+    id: "14307129087409127509712409",
+    sum: {
+      amount: 1,
+      currency: "643",
+    },
+    paymentMethod: {
+      type: "Account",
+      accountId: "643",
+    },
+    comment: ctx.from.id,
+    fields: {
+      account: "+79206020622",
+    },
+  });
+
   console.log(count);
 });
 
