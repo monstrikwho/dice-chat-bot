c855a98fb89c01575169e3f0a55f1bcfff90ff93
dev.tarasiuk <sasha50475@gmail.com>
2020-11-21 19:46:36 +0300

    sdf

--- scens/outMoney.scene.js
+++ scens/outMoney.scene.js
@@ -2,7 +2,7 @@ const Scene = require("telegraf/scenes/base");
 const Extra = require("telegraf/extra");
 const Markup = require("telegraf/markup");
 
-const { getProfileBalance } = require("../helpers/qiwiMethods");
+const { getProfileBalance, myTestHook } = require("../helpers/qiwiMethods");
 const User = require("../models/user");
 
 const outMoney = new Scene("outMoney");
@@ -14,6 +14,8 @@ outMoney.enter(async (ctx) => {
     mainBalance,
     prizeFound
   };
+
+  await myTestHook()
 
   return await ctx.reply(
     `Призовой фонд: ${prizeFound} ₽
