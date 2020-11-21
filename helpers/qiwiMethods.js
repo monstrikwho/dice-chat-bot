8ed0043fe2ea64a7a300047e999455f0e96daa8d
dev.tarasiuk <sasha50475@gmail.com>
2020-11-21 23:09:06 +0300

    выа

--- helpers/qiwiMethods.js
+++ helpers/qiwiMethods.js
@@ -143,7 +143,7 @@ module.exports.myTestHook = async () => {
   // Мой запрос сервер-сервер
   await axios
     .post(
-      `https://dice-bots.ru/verify_pay/`,
+      `https://dice-bots.ru/`,
       querystring.stringify({ sdfds: "sdfds" })
       // {
       // httpAgent: new http.Agent({ keepAlive: true }),
