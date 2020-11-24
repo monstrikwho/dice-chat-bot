8dd6801deb1a8b1a0a116fec494fd99061b31af8
dev.tarasiuk <sasha50475@gmail.com>
2020-11-24 17:27:14 +0300

    ЫАЫА

--- init/startRoutes.js
+++ init/startRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
 // app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.post("/verify_pay", (req, res) => {
   
