5087c80856e711060c230ff47a599a667738f77c
dev.tarasiuk <sasha50475@gmail.com>
2020-11-24 12:57:18 +0300

    ыва

--- init/startRoutes.js
+++ init/startRoutes.js
@@ -7,9 +7,10 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 
 app.post("/verify_pay", (req, res) => {
+  
   console.log(123); // Call your action on the request here
-  console.log(req.body); // Call your action on the request here
-  res.status(200).end(); // Responding is important
+  // console.log(req.body); // Call your action on the request here
+  // res.status(200).end(); // Responding is important
 });
 
 async function startRoutes() {
