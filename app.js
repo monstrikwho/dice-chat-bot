bf9244f40997b43f92e28027052a35592e642f11
dev.tarasiuk <sasha50475@gmail.com>
2020-12-15 00:32:11 +0300

    чсм

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const { bot, startBot } = require("./init/startBot");
 const { app, startRoutes } = require("./init/startRoutes");
 const setupMongoose = require("./init/setupMongoose");
 
-// Helpers
-// const fakeOrders = require("./helpers/fakeOrders");
-
 // Commands
 const setupStart = require("./commands/start");
 const mailing = require("./commands/mailing");
@@ -20,6 +17,3 @@ setupMongoose();
 // Commands
 setupStart(bot);
 mailing(bot);
-
-// Helpers
-// fakeOrders(bot)
