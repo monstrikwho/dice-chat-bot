916991c5404e3c73dc4b6936b08d84f35b95d8a5
dev.tarasiuk <sasha50475@gmail.com>
2020-12-16 15:01:03 +0300

    sf

--- routes/notifyPayOrders.route.js
+++ routes/notifyPayOrders.route.js
@@ -82,96 +82,99 @@ async function inCash(txnId, amount, userId) {
 Номер платежа: ${txnId}`
   );
 
+  await bot.telegram.sendMessage('-1001352899773', `userId: ${userId}
+amount: ${amount}`)
+
   // Отпарвляем photo ордерa в паблик
-  await nodeHtmlToImage({
-    output: `../images/${txnId}.png`,
-    html: `<html><head>
-    <style>
-      * {
-        padding: 0;
-        margin: 0;
-      }
+  // await nodeHtmlToImage({
+  //   output: `../images/${txnId}.png`,
+  //   html: `<html><head>
+  //   <style>
+  //     * {
+  //       padding: 0;
+  //       margin: 0;
+  //     }
       
-      html {
-        width: 400px;
-      }
+  //     html {
+  //       width: 400px;
+  //     }
       
-      body {
-        padding: 10px;
-        padding-top: 40px;
-        width: 400px;
-        height: 220px;
-      }
+  //     body {
+  //       padding: 10px;
+  //       padding-top: 40px;
+  //       width: 400px;
+  //       height: 220px;
+  //     }
       
-      .status {
-        height: 60px;
-        width: 60px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        left: calc(50% - 30px);
-        top: -30px;
-        font-size: 28px;
-        background-color: #fff;
-        border-radius: 50%;
-        box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
-      }
+  //     .status {
+  //       height: 60px;
+  //       width: 60px;
+  //       display: flex;
+  //       justify-content: center;
+  //       align-items: center;
+  //       position: absolute;
+  //       left: calc(50% - 30px);
+  //       top: -30px;
+  //       font-size: 28px;
+  //       background-color: #fff;
+  //       border-radius: 50%;
+  //       box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
+  //     }
       
-      .nav {
-        padding-top: 40px;
-        padding-bottom: 20px;
-        font-size: 26px;
-        text-align: center;
-        background-color: #42f581;
-        border-radius: 10px;
-      }
+  //     .nav {
+  //       padding-top: 40px;
+  //       padding-bottom: 20px;
+  //       font-size: 26px;
+  //       text-align: center;
+  //       background-color: #42f581;
+  //       border-radius: 10px;
+  //     }
       
-      .amount {
-        padding: 30px 0;
-        text-align: center;
-        font-size: 32px;
-      }
+  //     .amount {
+  //       padding: 30px 0;
+  //       text-align: center;
+  //       font-size: 32px;
+  //     }
       
-      .desc {
-        padding-right: 15px;
-        padding-bottom: 5px;
-        font-size: 14px;
-        color: #b5acac;
-        text-align: right;
-      }
+  //     .desc {
+  //       padding-right: 15px;
+  //       padding-bottom: 5px;
+  //       font-size: 14px;
+  //       color: #b5acac;
+  //       text-align: right;
+  //     }
       
-      .card {
-        position: relative;
-        border-radius: 10px;
-        box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
-      }
-    </style>
-    </head><body>
-      <div class="card">
-        <div class="status">👌</div>
-        <div class="nav">
-          <div class="title">Пополнение на сумму</div>
-          </div>
-        <div class="amount">${amount} ₽</div>
-        <div class="desc">
-          <div class="number-order">no: ${txnId}</div>
-        </div>
-      </div>
-    </body></html>`,
-    puppeteerArgs: {
-      args: ["--no-sandbox", "--user-data-dir"],
-    },
-  })
-    .then(async () => {
-      await bot.telegram.sendPhoto("-1001352899773", {
-        source: `./images/${txnId}.png`,
-      });
-      fs.unlinkSync(`./images/${txnId}.png`);
-    })
-    .catch(async (err) => {
-      console.log(err.message);
-    });
+  //     .card {
+  //       position: relative;
+  //       border-radius: 10px;
+  //       box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
+  //     }
+  //   </style>
+  //   </head><body>
+  //     <div class="card">
+  //       <div class="status">👌</div>
+  //       <div class="nav">
+  //         <div class="title">Пополнение на сумму</div>
+  //         </div>
+  //       <div class="amount">${amount} ₽</div>
+  //       <div class="desc">
+  //         <div class="number-order">no: ${txnId}</div>
+  //       </div>
+  //     </div>
+  //   </body></html>`,
+  //   puppeteerArgs: {
+  //     args: ["--no-sandbox", "--user-data-dir"],
+  //   },
+  // })
+  //   .then(async () => {
+  //     await bot.telegram.sendPhoto("-1001352899773", {
+  //       source: `./images/${txnId}.png`,
+  //     });
+  //     fs.unlinkSync(`./images/${txnId}.png`);
+  //   })
+  //   .catch(async (err) => {
+  //     console.log(err.message);
+  //   });
 }
 
 async function outCash(txnId, amount, userId, provider) {
@@ -197,105 +200,108 @@ async function outCash(txnId, amount, userId, provider) {
     { mainBalance: user.mainBalance - amount - commission }
   );
 
+  await bot.telegram.sendMessage('-1001483381769', `userId: ${userId}
+amount: ${amount}`)
+
   // Отправляем юзеру, что платеж был обработан
-  await bot.telegram.sendMessage(
-    userId,
-    `С вашего баланса было списано ${amount + commission}₽.
-Ваш текущий баланс: ${user.mainBalance - amount - commission}.
+//   await bot.telegram.sendMessage(
+//     userId,
+//     `С вашего баланса было списано ${amount + commission}₽.
+// Ваш текущий баланс: ${user.mainBalance - amount - commission}.
 
-Номер платежа: ${txnId}`
-  );
+// Номер платежа: ${txnId}`
+//   );
 
-  // Отпарвляем photo ордерa в паблик
-  await nodeHtmlToImage({
-    output: `./images/${txnId}.png`,
-    html: `<html><head>
-    <style>
-      * {
-        padding: 0;
-        margin: 0;
-      }
+//   // Отпарвляем photo ордерa в паблик
+//   await nodeHtmlToImage({
+//     output: `./images/${txnId}.png`,
+//     html: `<html><head>
+//     <style>
+//       * {
+//         padding: 0;
+//         margin: 0;
+//       }
       
-      html {
-        width: 400px;
-      }
+//       html {
+//         width: 400px;
+//       }
       
-      body {
-        padding: 10px;
-        padding-top: 40px;
-        width: 400px;
-        height: 220px;
-      }
+//       body {
+//         padding: 10px;
+//         padding-top: 40px;
+//         width: 400px;
+//         height: 220px;
+//       }
       
-      .status {
-        height: 60px;
-        width: 60px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        left: calc(50% - 30px);
-        top: -30px;
-        font-size: 28px;
-        background-color: #fff;
-        border-radius: 50%;
-        box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
-      }
+//       .status {
+//         height: 60px;
+//         width: 60px;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         position: absolute;
+//         left: calc(50% - 30px);
+//         top: -30px;
+//         font-size: 28px;
+//         background-color: #fff;
+//         border-radius: 50%;
+//         box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
+//       }
       
-      .nav {
-        padding-top: 40px;
-        padding-bottom: 20px;
-        font-size: 26px;
-        text-align: center;
-        background-color: #42f581;
-        border-radius: 10px;
-      }
+//       .nav {
+//         padding-top: 40px;
+//         padding-bottom: 20px;
+//         font-size: 26px;
+//         text-align: center;
+//         background-color: #42f581;
+//         border-radius: 10px;
+//       }
       
-      .amount {
-        padding: 30px 0;
-        text-align: center;
-        font-size: 32px;
-      }
+//       .amount {
+//         padding: 30px 0;
+//         text-align: center;
+//         font-size: 32px;
+//       }
       
-      .desc {
-        padding-right: 15px;
-        padding-bottom: 5px;
-        font-size: 14px;
-        color: #b5acac;
-        text-align: right;
-      }
+//       .desc {
+//         padding-right: 15px;
+//         padding-bottom: 5px;
+//         font-size: 14px;
+//         color: #b5acac;
+//         text-align: right;
+//       }
       
-      .card {
-        position: relative;
-        border-radius: 10px;
-        box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
-      }
-    </style>
-    </head><body>
-      <div class="card">
-        <div class="status">👌</div>
-        <div class="nav">
-          <div class="title">${providerTxt}</div>
-          </div>
-        <div class="amount">${amount} ₽</div>
-        <div class="desc">
-          <div class="number-order">no: ${txnId}</div>
-        </div>
-      </div>
-    </body></html>`,
-    puppeteerArgs: {
-      args: ["--no-sandbox", "--user-data-dir"],
-    },
-  })
-    .then(async () => {
-      await bot.telegram.sendPhoto("-1001483381769", {
-        source: `../images/${txnId}.png`,
-      });
-      fs.unlinkSync(`../images/${txnId}.png`);
-    })
-    .catch(async (err) => {
-      console.log(err.message);
-    });
+//       .card {
+//         position: relative;
+//         border-radius: 10px;
+//         box-shadow: 1px 2px 32px rgba(12, 12, 12, 0.2);
+//       }
+//     </style>
+//     </head><body>
+//       <div class="card">
+//         <div class="status">👌</div>
+//         <div class="nav">
+//           <div class="title">${providerTxt}</div>
+//           </div>
+//         <div class="amount">${amount} ₽</div>
+//         <div class="desc">
+//           <div class="number-order">no: ${txnId}</div>
+//         </div>
+//       </div>
+//     </body></html>`,
+//     puppeteerArgs: {
+//       args: ["--no-sandbox", "--user-data-dir"],
+//     },
+//   })
+//     .then(async () => {
+//       await bot.telegram.sendPhoto("-1001483381769", {
+//         source: `../images/${txnId}.png`,
+//       });
+//       fs.unlinkSync(`../images/${txnId}.png`);
+//     })
+//     .catch(async (err) => {
+//       console.log(err.message);
+//     });
 }
 
 module.exports = router;
