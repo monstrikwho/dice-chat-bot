ec155352ae7754416fab8d16352d52eb0a6121d4
dev.tarasiuk <sasha50475@gmail.com>
2020-12-16 14:47:23 +0300

    ыва

--- commands/start.js
+++ commands/start.js
@@ -7,58 +7,59 @@ function setupStart(bot) {
   // Setup scens
   setupScenes(bot);
 
-  const actionsUser = async (ctx) => {
-    // Сохраняем статистику рекламы
-    const startPayload = ctx.startPayload;
-    if (startPayload.length > 0) {
-      const { payload } = await Setting.findOne({});
-      if (payload[startPayload]) {
-        await Setting.updateOne(
-          {},
-          { payload: { ...payload, [startPayload]: payload[startPayload] + 1 } }
-        );
-      } else {
-        await Setting.updateOne(
-          {},
-          { payload: { ...payload, [startPayload]: 1 } }
-        );
-      }
-    }
-
-    const selectUser = await User.findOne({ userId: ctx.from.id });
-    if (!selectUser) {
-      try {
-        const user = new User({
-          userId: ctx.from.id,
-          demoBalance: 1000,
-          mainBalance: 0,
-          regDate: moment().format("DD, MM, YYYY, hh:mm:ss"),
-          userName: ctx.from.username,
-          isBlocked: false,
-        });
-        await user.save();
-      } catch (error) {
-        console.log(error.message);
-      }
-    } else {
-      await User.findOne({ userId: ctx.from.id }, { isBlocked: false });
-      return await ctx.scene.enter("showMainMenu");
-    }
-
-    await ctx.reply(`Добро пожаловать в бот честных онлайн игр!
-  Здесь удача зависит только от вас!🌈
-  
-  Вы сами отправляете нам игровой стикер от телеграмм, а мы считываем его результат и платим Вам деньги! 💸
-  
-  Попробуйте БЕСПЛАТНО на демо-счете. Приятной игры!🎉`);
-    return await ctx.scene.enter("showMainMenu");
-  };
-
   // Start command
   bot.start(async (ctx) => {
     if (+ctx.chat.id < 0) return;
     try {
-      setTimeout(() => actionsUser(ctx), 0);
+      // Сохраняем статистику рекламы
+      const startPayload = ctx.startPayload;
+      if (startPayload.length > 0) {
+        const { payload } = await Setting.findOne({});
+        if (payload[startPayload]) {
+          await Setting.updateOne(
+            {},
+            {
+              payload: {
+                ...payload,
+                [startPayload]: payload[startPayload] + 1,
+              },
+            }
+          );
+        } else {
+          await Setting.updateOne(
+            {},
+            { payload: { ...payload, [startPayload]: 1 } }
+          );
+        }
+      }
+
+      const selectUser = await User.findOne({ userId: ctx.from.id });
+      if (!selectUser) {
+        try {
+          const user = new User({
+            userId: ctx.from.id,
+            demoBalance: 1000,
+            mainBalance: 0,
+            regDate: moment().format("DD, MM, YYYY, hh:mm:ss"),
+            userName: ctx.from.username,
+            isBlocked: false,
+          });
+          await user.save();
+        } catch (error) {
+          console.log(error.message);
+        }
+      } else {
+        await User.findOne({ userId: ctx.from.id }, { isBlocked: false });
+        return await ctx.scene.enter("showMainMenu");
+      }
+
+      await ctx.reply(`Добро пожаловать в бот честных онлайн игр!
+Здесь удача зависит только от вас!🌈
+
+Вы сами отправляете нам игровой стикер от телеграмм, а мы считываем его результат и платим Вам деньги! 💸
+
+Попробуйте БЕСПЛАТНО на демо-счете. Приятной игры!🎉`);
+      return await ctx.scene.enter("showMainMenu");
     } catch (err) {
       console.log("Не удалось пройти регистрацию", err.message);
     }
