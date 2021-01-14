1535063587531a3a93c07a56e22c1eb74e04f6db
dev.tarasiuk <sasha50475@gmail.com>
2021-01-14 12:22:55 +0300

    sdf

--- models/setting.js
+++ models/setting.js
@@ -1,21 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
-  lastNumberOrder: {
-    type: Number,
-  },
-  footballGame: {
-    type: Object
-  },
-  slotGame: {
-    type: Object
-  },
-  countBlocked: {
-    type: Number
-  },
-  payload: {
-    type: Object
-  }
+  lastNumberOrder: String,
+  countBlocked: Number,
+  payload: Object,
 });
 
 module.exports = model("Settings", schema);
