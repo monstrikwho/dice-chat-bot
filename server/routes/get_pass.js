b3cb3670bfc9b580511554c59e2201b99acd8313
dev.tarasiuk <sasha50475@gmail.com>
2021-03-24 10:09:19 +0300

    не убрал переменную

--- server/routes/get_pass.js
+++ server/routes/get_pass.js
@@ -3,20 +3,19 @@ const router = Router();
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const MainStats = require("../models/mainstats");
 
 router.get("/", async (req, res) => {
   const { pass } = await User.findOne({
     userId: req.query.login,
   });
 
+  const { webhook } = await MainStats.findOne({});
+
   if (pass == req.query.pass) {
-    const token = jwt.sign(
-      { userId: req.query.login },
-      process.env.AUTH_SECRET_KEY,
-      {
-        expiresIn: 60 * 60 * 5,
-      }
-    );
+    const token = jwt.sign({ userId: req.query.login }, webhook.authSecret, {
+      expiresIn: 60 * 60 * 5,
+    });
     res.send({ status: true, token });
   } else {
     return res.send({ status: false });
