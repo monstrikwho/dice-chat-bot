ea0e6f74bc9c93900e9247778dceb92acf7ef7b7
dev.tarasiuk <sasha50475@gmail.com>
2021-04-28 21:03:28 +0300

    delete

--- server/init/setupMongoose.js
+++ server/init/setupMongoose.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
+
 module.exports = () => {
   mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-  })
-  // Reconnect on disconnect
-  mongoose.connection.on('disconnected', () => {
-    mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-  })
-}
+  });
+};
