e3d3b7f1854b7bbd717898a389ed4c96dfaa3dff
dev.tarasiuk <sasha50475@gmail.com>
2021-08-06 11:34:38 +0300

    12

--- server/app.js
+++ server/app.js
@@ -19,195 +19,191 @@ setupStart(bot);
 setupStats(bot);
 updatePvpStats();
 
-const axios = require("axios");
-const moment = require("moment");
-const SportsTopGames = require("./models/sportsTopGames");
-const SportFootballGames = require("./models/sportFootballGames");
+// const axios = require("axios");
+// const moment = require("moment");
+// const SportsTopGames = require("./models/sportsTopGames");
+// const SportFootballGames = require("./models/sportFootballGames");
 
-const { create, all } = require("mathjs");
-const math = create(all);
+// const { create, all } = require("mathjs");
+// const math = create(all);
 
-const login = "tarasiuk";
-const token = "02185-7p3inJMv7HsfsXK";
+// const login = "tarasiuk";
+// const token = "02185-7p3inJMv7HsfsXK";
 
-const chunks = 3;
-const typesMatch = ["pre"];
-const sports = ["soccer"];
+// const chunks = 3;
+// const typesMatch = ["pre"];
+// const sports = ["soccer"];
 // const typesMatch = ["live", "pre"];
 // const sports = ["soccer", "basketball", "tennis"];
 
-setInterval(() => {
-  for (let i = 0; i < sports.length; i++) {
-    for (let k = 0; k < typesMatch.length; k++) {
-      req(sports[i], typesMatch[k]);
-    }
-  }
-  async function req(sport, type) {
-    await axios
-      .get(
-        `https://spoyer.ru/api/get.php?login=${login}&token=${token}&task=${type}&bookmaker=bet365&sport=${sport}`
-      )
-      .then(async ({ data }) => {
-        const timeReq = +data.time_request.toFixed(2);
-        const allGames = type === "live" ? data.games_live : data.games_pre;
-        const topGames = allGames.slice(0, chunks);
+// setInterval(() => {
+//   for (let i = 0; i < sports.length; i++) {
+//     for (let k = 0; k < typesMatch.length; k++) {
+//       req(sports[i], typesMatch[k]);
+//     }
+//   }
+//   async function req(sport, type) {
+//     await axios
+//       .get(
+//         `https://spoyer.ru/api/get.php?login=${login}&token=${token}&task=${type}&bookmaker=bet365&sport=${sport}`
+//       )
+//       .then(async ({ data }) => {
+//         const timeReq = +data.time_request.toFixed(2);
+//         const allGames = type === "live" ? data.games_live : data.games_pre;
+//         const topGames = allGames.slice(0, chunks);
 
-        const status = await SportsTopGames.findOne({
-          sport: `${sport}_${type}`,
-        });
+//         const status = await SportsTopGames.findOne({
+//           sport: `${sport}_${type}`,
+//         });
 
-        if (!status) {
-          const newGame = new SportsTopGames({
-            sport: `${sport}_${type}`,
-            topGames,
-            timeUpd: moment().format("HH:mm:ss"),
-            timeReq,
-          });
-          await newGame.save();
-        }
+//         if (!status) {
+//           const newGame = new SportsTopGames({
+//             sport: `${sport}_${type}`,
+//             topGames,
+//             timeUpd: moment().format("HH:mm:ss"),
+//             timeReq,
+//           });
+//           await newGame.save();
+//         }
 
-        await SportsTopGames.updateOne(
-          { sport: `${sport}_${type}` },
-          { topGames, timeUpd: moment().format("HH:mm:ss"), timeReq }
-        );
+//         await SportsTopGames.updateOne(
+//           { sport: `${sport}_${type}` },
+//           { topGames, timeUpd: moment().format("HH:mm:ss"), timeReq }
+//         );
 
-        console.log(`Parse matches ${sport}_${type}`);
-      })
-      .catch((err) => console.log(err.message));
-  }
-}, 1000 * 60 * 1566);
+//         console.log(`Parse matches ${sport}_${type}`);
+//       })
+//       .catch((err) => console.log(err.message));
+//   }
+// }, 1000 * 60 * 1566);
 
-setInterval(async () => {
-  for (let i = 0; i < sports.length; i++) {
-    for (let type of typesMatch) {
-      const { topGames } = await SportsTopGames.findOne({
-        sport: `${sports[i]}_${type}`,
-      });
-      for (let y = 0; y < topGames.length; y++) {
-        const game = topGames[y];
-        req(sports[i], type, game);
-      }
-    }
-  }
+// setInterval(async () => {
+//   for (let i = 0; i < sports.length; i++) {
+//     for (let type of typesMatch) {
+//       const { topGames } = await SportsTopGames.findOne({
+//         sport: `${sports[i]}_${type}`,
+//       });
+//       for (let y = 0; y < topGames.length; y++) {
+//         const game = topGames[y];
+//         req(sports[i], type, game);
+//       }
+//     }
+//   }
 
-  async function req(sport, type, game) {
-    await axios
-      .get(
-        `https://spoyer.ru/api/get.php?login=${login}&token=${token}&task=${type}odds&bookmaker=bet365&game_id=${game.game_id}`
-      )
-      .then(async ({ data }) => {
-        if (sport === "soccer") {
-          saveSoccerData(data, game, type);
-        }
-      })
-      .catch((err) => console.log(err.message));
-  }
+//   async function req(sport, type, game) {
+//     await axios
+//       .get(
+//         `https://spoyer.ru/api/get.php?login=${login}&token=${token}&task=${type}odds&bookmaker=bet365&game_id=${game.game_id}`
+//       )
+//       .then(async ({ data }) => {
+//         if (sport === "soccer") {
+//           saveSoccerData(data, game, type);
+//         }
+//       })
+//       .catch((err) => console.log(err.message));
+//   }
 
-  async function saveSoccerData(data, game, type) {
-    if (type === "live") {
-      // голы матча NA: 'Match Goals'
-      // aльтернативные голы матча NA: 'Alternative Match Goals'
-      // голы чет/нечет NA: 'Goals Odd/Even'
-      // обе команды забьют NA: 'Both Teams to Score'
-      // const p1 = +(math.evaluate(data.results[0][37].OD) + 1).toFixed(2);
-      // const draw = +(math.evaluate(data.results[0][38].OD) + 1).toFixed(2);
-      // const p2 = +(math.evaluate(data.results[0][39].OD) + 1).toFixed(2);
-      // const update_at = moment
-      //   .unix(data.stats.update_at)
-      //   .utc()
-      //   .local()
-      //   .format("HH:mm:ss");
-      // const status = await SportFootballGames.findOne({
-      //   game_id: +game.game_id,
-      // });
-      // if (!status) {
-      //   const newGame = new SportFootballGames({
-      //     ...game,
-      //     odds: {
-      //       p1,
-      //       draw,
-      //       p2,
-      //     },
-      //     update_at,
-      //     parse_time: moment().format("HH:mm:ss"),
-      //   });
-      //   await newGame.save();
-      // }
-      // await SportFootballGames.updateOne(
-      //   { game_id: +game.game_id },
-      //   {
-      //     odds: { p1, draw, p2 },
-      //     update_at,
-      //     parse_time: moment().format("HH:mm:ss"),
-      //   }
-      // );
-    }
+//   async function saveSoccerData(data, game, type) {
+//     if (type === "live") {
+//       // голы матча NA: 'Match Goals'
+//       // aльтернативные голы матча NA: 'Alternative Match Goals'
+//       // голы чет/нечет NA: 'Goals Odd/Even'
+//       // обе команды забьют NA: 'Both Teams to Score'
+//       // const p1 = +(math.evaluate(data.results[0][37].OD) + 1).toFixed(2);
+//       // const draw = +(math.evaluate(data.results[0][38].OD) + 1).toFixed(2);
+//       // const p2 = +(math.evaluate(data.results[0][39].OD) + 1).toFixed(2);
+//       // const update_at = moment
+//       //   .unix(data.stats.update_at)
+//       //   .utc()
+//       //   .local()
+//       //   .format("HH:mm:ss");
+//       // const status = await SportFootballGames.findOne({
+//       //   game_id: +game.game_id,
+//       // });
+//       // if (!status) {
+//       //   const newGame = new SportFootballGames({
+//       //     ...game,
+//       //     odds: {
+//       //       p1,
+//       //       draw,
+//       //       p2,
+//       //     },
+//       //     update_at,
+//       //     parse_time: moment().format("HH:mm:ss"),
+//       //   });
+//       //   await newGame.save();
+//       // }
+//       // await SportFootballGames.updateOne(
+//       //   { game_id: +game.game_id },
+//       //   {
+//       //     odds: { p1, draw, p2 },
+//       //     update_at,
+//       //     parse_time: moment().format("HH:mm:ss"),
+//       //   }
+//       // );
+//     }
 
-    if (type === "pre") {
-      const p1 = data.results[0]?.schedule?.sp?.main[0]?.odds || "";
-      const draw = data.results[0]?.schedule?.sp?.main[1]?.odds || "";
-      const p2 = data.results[0]?.schedule?.sp?.main[2]?.odds || "";
-      const goals_over_under =
-        data.results[0]?.goals?.sp?.goals_over_under || [];
-      const alternative_total_goals =
-        data.results[0]?.goals?.sp?.alternative_total_goals || [];
-      const goals_odd_even = data.results[0]?.goals?.sp?.goals_odd_even || [];
-      const both_teams_to_score =
-        data.results[0]?.goals?.sp?.both_teams_to_score || [];
+//     if (type === "pre") {
+//       const p1 = data.results[0]?.schedule?.sp?.main[0]?.odds || "";
+//       const draw = data.results[0]?.schedule?.sp?.main[1]?.odds || "";
+//       const p2 = data.results[0]?.schedule?.sp?.main[2]?.odds || "";
+//       const goals_over_under =
+//         data.results[0]?.goals?.sp?.goals_over_under || [];
+//       const alternative_total_goals =
+//         data.results[0]?.goals?.sp?.alternative_total_goals || [];
+//       const goals_odd_even = data.results[0]?.goals?.sp?.goals_odd_even || [];
+//       const both_teams_to_score =
+//         data.results[0]?.goals?.sp?.both_teams_to_score || [];
 
-      const update_at =
-        (data.results[0]?.schedule?.updated_at ??
-          moment
-            .unix(data.results[0]?.schedule?.updated_at)
-            .utc()
-            .local()
-            .format("HH:mm:ss")) ||
-        "";
+//       const update_at =
+//         (data.results[0]?.schedule?.updated_at ??
+//           moment
+//             .unix(data.results[0]?.schedule?.updated_at)
+//             .utc()
+//             .local()
+//             .format("HH:mm:ss")) ||
+//         "";
 
-      const status = await SportFootballGames.findOne({
-        game_id: +game.game_id,
-      });
+//       const status = await SportFootballGames.findOne({
+//         game_id: +game.game_id,
+//       });
 
-      if (!status) {
-        const newGame = new SportFootballGames({
-          ...game,
-          odds: {
-            p1,
-            draw,
-            p2,
-            goals_over_under,
-            alternative_total_goals,
-            goals_odd_even,
-            both_teams_to_score,
-          },
-          update_at,
-          parse_time: moment().format("HH:mm:ss"),
-        });
-        await newGame.save();
-      }
+//       if (!status) {
+//         const newGame = new SportFootballGames({
+//           ...game,
+//           odds: {
+//             p1,
+//             draw,
+//             p2,
+//             goals_over_under,
+//             alternative_total_goals,
+//             goals_odd_even,
+//             both_teams_to_score,
+//           },
+//           update_at,
+//           parse_time: moment().format("HH:mm:ss"),
+//         });
+//         await newGame.save();
+//       }
 
-      await SportFootballGames.updateOne(
-        { game_id: +game.game_id },
-        {
-          odds: {
-            p1,
-            draw,
-            p2,
-            goals_over_under,
-            alternative_total_goals,
-            goals_odd_even,
-            both_teams_to_score,
-          },
-          update_at,
-          parse_time: moment().format("HH:mm:ss"),
-        }
-      );
+//       await SportFootballGames.updateOne(
+//         { game_id: +game.game_id },
+//         {
+//           odds: {
+//             p1,
+//             draw,
+//             p2,
+//             goals_over_under,
+//             alternative_total_goals,
+//             goals_odd_even,
+//             both_teams_to_score,
+//           },
+//           update_at,
+//           parse_time: moment().format("HH:mm:ss"),
+//         }
+//       );
 
-      console.log(`Parse one match n.${game.game_id}`);
-    }
-  }
-}, 1000 * 60 * 5556);
-
-bot.on("photo", (ctx) => {
-  console.log(ctx.update.message.photo);
-});
+//       console.log(`Parse one match n.${game.game_id}`);
+//     }
+//   }
+// }, 1000 * 60 * 5556);
--- server/helpers/updatePvpStats.js
+++ server/helpers/updatePvpStats.js
@@ -9,43 +9,14 @@ async function updatePvpStats() {
     const time = moment().format("HH-mm");
     if (time !== "02-00") return;
 
-    // Рейтинг КОСТИ
-    const diceUsers = await User.find({ "pvpDice.count": { $gte: 1 } }).sort({
-      "pvpDice.winCash": -1,
+    const diceUsers = await User.find({ "pvp.count": { $gte: 1 } }).sort({
+      "pvp.winCash": -1,
     });
 
     for (let i = 0; i < diceUsers.length; i++) {
       await User.updateOne(
         { userId: diceUsers[i].userId },
-        { "pvpDice.rating": i + 1 }
-      );
-    }
-
-    // Рейтинг ФУТБОЛ
-    const footballUsers = await User.find({
-      "pvpFootball.count": { $gte: 1 },
-    }).sort({
-      "pvpFootball.winCash": -1,
-    });
-
-    for (let i = 0; i < footballUsers.length; i++) {
-      await User.updateOne(
-        { userId: footballUsers[i].userId },
-        { "pvpFootball.rating": i + 1 }
-      );
-    }
-
-    // Рейтинг БОУЛИНГ
-    const boulingUsers = await User.find({
-      "pvpBouling.count": { $gte: 1 },
-    }).sort({
-      "pvpBouling.winCash": -1,
-    });
-
-    for (let i = 0; i < boulingUsers.length; i++) {
-      await User.updateOne(
-        { userId: boulingUsers[i].userId },
-        { "pvpBouling.rating": i + 1 }
+        { "pvp.rating": i + 1 }
       );
     }
   }
