c1c043f525e6d6086bbe0bc211d63c02b93302f7
dev.tarasiuk <sasha50475@gmail.com>
2021-09-04 00:29:12 +0300

    123

--- server/init/telegram.js
+++ server/init/telegram.js
@@ -1,5 +1,6 @@
 const { Api, TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
+const input = require("input");
 
 const { bot } = require("./startBot");
 const Banker = require("../models/banker");
@@ -7,16 +8,20 @@ const User = require("../models/user");
 
 const apiId = 7548033;
 const apiHash = "eb38dcae569b88f3ad6d6cdc9fd97038";
-const stringSession = new StringSession(
-  "1AQAOMTQ5LjE1NC4xNjcuNTEAUF3T4erW+NsMWeAPcbE4tKMyzPRiaco+WXLBZ06LyMzzUANxrQ0eCgqH4AlHH2NcwgXcAT0mpH/g8zDoRyIWJmdg0Ff6U0tmyGS5amunCQ6lV0zFeEWNWKNLIpSwRUpnV4K6jPIMLav0ey0ojM1d9viPARs/1AVtkfoFa21NNAxGaC7TE/OO0JivlU30sjvIoocvPKs7EOPfwp7dcTDXVQrVZHhAEnVgLbxkTGmaCuQ0k7iDstrsO5pqz/Oqt+6t+ZJTtLOcdp9zYY2Dehf8646y6fyi6lp/3v8fF/EydiXFBrd8S/7GA7v2344geh2FjMpLIHU31oBxSSpALoqffAQ="
-);
+const stringSession = new StringSession("");
 
 const client = new TelegramClient(stringSession, apiId, apiHash, {
   connectionRetries: 5,
 });
 
 (async function run() {
-  await client.connect();
+  await client.start({
+    phoneNumber: async () => await input.text("Please enter your number: "),
+    password: async () => await input.text("Please enter your password: "),
+    phoneCode: async () =>
+      await input.text("Please enter the code you received: "),
+    onError: (err) => console.log(err),
+  });
 
   client.addEventHandler(eventPrint);
   async function eventPrint(event) {
